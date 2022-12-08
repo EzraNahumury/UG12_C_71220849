fd1fcbff87308215c60d2240888e65b3c1e4c2d6
EzraNahumury <117893407+EzraNahumury@users.noreply.github.com>
2022-12-08 15:59:23 +0700

    Add files via upload

---
+++ 1_C_71220849.py
@@ -0,0 +1,26 @@
+kata = input("Masukkan kata atau angka: ")
+
+kata_terbalik = ""
+
+for i in range(len(kata)-1, -1, -1):
+    kata_terbalik += kata[i]
+
+print(kata_terbalik)
+
+kata = input("Masukkan kata atau angka: ")
+
+kata_terbalik = ""
+
+for i in range(len(kata)-1, -1, -1):
+    kata_terbalik += kata[i]
+
+print(kata_terbalik)
+
+kata = input("Masukkan kata atau angka: ")
+
+kata_terbalik = ""
+
+for i in range(len(kata)-1, -1, -1):
+    kata_terbalik += kata[i]
+
+print(kata_terbalik)
---
+++ 2_C_71220849.py
@@ -0,0 +1,43 @@
+
+baris = input("Masukkan angka: ")
+
+angka = input("Masukkan angka yang dihitung: ")
+
+jumlah = 0
+
+for i in range(len(baris)):
+  
+  if baris[i] == angka:
+    jumlah += 1
+
+print("angka {} mucul sebanyak {}kali".format(angka, jumlah))
+
+
+baris = input("Masukkan angka: ")
+
+angka = input("Masukkan angka yang dihitung: ")
+
+jumlah = 0
+
+for i in range(len(baris)):
+  
+  if baris[i] == angka:
+    jumlah += 1
+
+print("angka {} mucul sebanyak {}kali".format(angka, jumlah))
+
+
+baris = input("Masukkan angka: ")
+
+angka = input("Masukkan angka yang dihitung: ")
+
+jumlah = 0
+
+for i in range(len(baris)):
+  
+  if baris[i] == angka:
+    jumlah += 1
+
+print("angka {} mucul sebanyak {}kali".format(angka, jumlah))
+
+
